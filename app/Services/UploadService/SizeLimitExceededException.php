6424d62ae31a251ddbae231de55a909907d70dc4
paulsnar <paulsnar@paulsnar.lv>
2022-03-28 01:10:04 +0300

    fix: Move SizeLimitExceededException to its proper namespace
    It was Composer who caught this, oddly enough, when regenerating the
    optimized autoloader.

--- app/Services/UploadService/SizeLimitExceededException.php
+++ app/Services/UploadService/SizeLimitExceededException.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-namespace App\Services\UploadService\SizeLimitExceededException;
+namespace App\Services\UploadService;
 
 class SizeLimitExceededException extends \RuntimeException
 {
