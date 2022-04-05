untitled-pit-group/foxhound

commit 522ea18d01add32047ec33a3727cbbc7d8608d26

feat: Add parts of UploadService

app/Repositories/UploadRepo.php
@@ -1,0 +1,42 @@
+<?php declare(strict_types=1);
+namespace App\Repositories\UploadRepo;
+use App\Models\Upload;
+use App\Services\GcloudStorageService\GcsUrl;
+use App\Support\Sha1Hash;
+use App\Support\Mixin\OptionallyBeginsTransaction;
+use Illuminate\Database\DatabaseTransactionsManager;
+use Illuminate\Support\Carbon;
+
+class UploadRepo
+{
+    use OptionallyBeginsTransaction;
+    public function __construct(DatabaseTransactionsManager $dtm)
+    {
+        $this->databaseTransactionsManager = $dtm;
+    }
+
+    public function createEmpty(
+        Sha1Hash $hash,
+        int $length,
+        GcsUrl $gcsPath,
+        string $name,
+    ): Upload {
+        $params = compact('hash', 'length', 'gcsPath', 'name');
+        return $this->inTransaction(
+            function () use ($hash, $length, $gcsPath, $name) {
+                $upload = new Upload();
+                $upload->hash = $hash;
+                $upload->length = $length;
+                $upload->gcs_path = $gcsPath;
+                $upload->name = $name;
+                $upload->upload_start = new Carbon('now');
+                $uplaod->progress = 0.0;
+                $upload->last_progress_report = null;
+
+                $upload->generateId();
+                $upload->save();
+
+                return $upload;
+            });
+    }
+}

app/Services/UploadService.php
@@ -1,35 +1,76 @@
 <?php declare(strict_types=1);
 namespace App\Services;
+use App\Models\{File, Upload};
 use App\Repositories\UploadRepo;
+use App\Services\GcloudStorageService\GcsUrl;
 use App\Services\UploadService\{AlreadyUploadedException,
     SizeLimitExceededException, UploadInProgressException};
+use App\Support\Sha1Hash;
 
 class UploadService
 {
-    public function __construct(private GcloudStorageService $gcs) {}
+    // TODO[pn]: This should be configurable.
+    const MAX_UPLOAD_SIZE_BYTES = 8 * 1024 * 1024 * 1024; // 8 GiB
+
+    public function __construct(
+        private GcloudStorageService $gcs,
+        private UploadRepo $uploads,
+    ) {}
+
+    protected function hashToGcsUrl(Sha1Hash $hash): GcsUrl
+    {
+        return $this->gcs->relativePathToAbsolutePath($hash->hex());
+    }
 
     /**
+     * @throws AlreadyUploadedException
+     * @throws SizeLimitExceededException
+     * @throws UploadInProgressException
      * @return [Upload, string $url]
      */
-    public function begin(string $hash, int $length, string $name): array
+    public function begin(Sha1Hash $hash, int $length, string $name): array
     {
-        throw new \App\Support\NotImplementedException();
+        if ($length > self::MAX_UPLOAD_SIZE_BYTES) {
+            throw new SizeLimitExceededException();
+        }
 
-        $this->checkHashConflicts($hash, $length);
-        // TODO: Check whether the size doesn't exceed the configured limit.
+        $url = $this->hashToGcsUrl($hash);
+
+        $upload = DB::transaction(function () use ($hash, $length, $name, $url) {
+            $this->checkHashConflicts($hash, $length);
+            return $this->uploads->createEmpty(
+                hash: $hash,
+                length: $length,
+                gcsPath: $url,
+                name: $name,
+            );
+
+            $url = $this->gcs->signedUploadUrl($url);
+            return [$upload, $url];
+        });
+
+        $url = $this->gcs->signedUploadUrl($url);
+        return [$upload, $url]
         // TODO: Make a new Upload with the relevant data and persist it. Don't
         // forget to set its ID properly.
         // TODO: Generate the GCS URL based on the config param.
         return [$upload, $url];
     }
 
-    protected function checkHashConflicts(string $hash, int $length): void
+    protected function checkHashConflicts(Sha1Hash $hash, int $length): void
     {
-        // TODO: Check whether a file with this hash hasn't already been uploaded.
-        // If so, throw an AlreadyUploadedException with the relevant File attached.
-        // If there length differs, a hash collision has occurred; I guess a new
-        // exception should be provisioned for that case?
-        // TODO: Check whether a file with this hash isn't currently being uploaded.
-        // If so, throw an UploadInProgressException with the relevant Upload.
+        $file = File::where('hash', $hash->raw)->first();
+        if ($file !== null) {
+            // TODO[pn]: hash collision
+            //if ($file->length !== $length) {
+            throw new AlreadyUploadedException($file);
+        }
+
+        $upload = Upload::where('hash', $hash->raw)->first();
+        if ($upload !== null) {
+            // TODO[pn]: hash collision
+            //if ($upload->length !== $length) {
+            throw new UploadInProgressException($upload);
+        }
     }
 }

app/Support/Mixin/OptionallyBeginsTransaction.php
@@ -1,0 +1,17 @@
+<?php declare(strict_types=1);
+namespace App\Support\Mixin;
+use Illuminate\Database\DatabaseTransactionsManager;
+
+trait OptionallyBeginsTransaction
+{
+    protected DatabaseTransactionsManager $databaseTransactionsManager;
+
+    protected function inTransaction(\Closure $callback)
+    {
+        if ($this->databaseTransactionsManager->getTransactions()->isEmpty()) {
+            return DB::transaction($callback);
+        } else {
+            return $callback();
+        }
+    }
+}

commit f9331833a248761f803687c0346ed1eb221e0a28

fix: Make uploads.begin work
Fix a bunch of stuff that was added in a broken state in 522ea18.
Notably, the DB facade isn't available so we gotta use app('db')
instead; and also the blob value has to be bytea-encoded instead of sent
over in binary form.

app/Http/Rpc/UploadController.php
@@ -4,7 +4,7 @@ use App\Rpc\RpcError;
 use App\Services\UploadService;
 use App\Services\UploadService\{AlreadyUploadedException,
     SizeLimitExceededException, UploadInProgressException};
-use App\Support\{Id, Math, NotImplementedException, RpcConstants};
+use App\Support\{Id, Math, NotImplementedException, RpcConstants, Sha1Hash};
 
 class UploadController
 {
@@ -24,7 +24,13 @@ class UploadController
                 "transmitted over JSON.");
         }
         try {
-            [$upload, $url] = $uploads->begin(
+            $hash = Sha1Hash::fromHex($hash);
+        } catch (\InvalidArgumentException $exc) {
+            throw new RpcError(RpcConstants::ERROR_INVALID_PARAMS,
+                "The hash provided is not a valid SHA-1 hash in hex format.");
+        }
+        try {
+            [$upload, $url] = $this->uploads->begin(
                 hash: $hash, length: $length, name: $name);
             return [
                 'upload_id' => Id::encode($upload->id),

app/Repositories/UploadRepo.php
@@ -27,7 +27,7 @@ class UploadRepo
         $upload->gcs_path = $gcsPath;
         $upload->name = $name;
         $upload->upload_start = new Carbon('now');
-        $uplaod->progress = 0.0;
+        $upload->progress = 0.0;
         $upload->last_progress_report = null;
         $this->inTransaction(function () use ($upload) {
             $upload->generateId();

app/Services/UploadService.php
@@ -36,7 +36,7 @@ class UploadService
 
         $url = $this->hashToGcsUrl($hash);
 
-        $upload = DB::transaction(function () use ($hash, $length, $name, $url) {
+        $upload = app('db')->transaction(function () use ($hash, $length, $name, $url) {
             $this->checkHashConflicts($hash, $length);
             return $this->uploads->createEmpty(
                 hash: $hash,
@@ -50,23 +50,19 @@ class UploadService
         });
 
         $url = $this->gcs->signedUploadUrl($url);
-        return [$upload, $url]
-        // TODO: Make a new Upload with the relevant data and persist it. Don't
-        // forget to set its ID properly.
-        // TODO: Generate the GCS URL based on the config param.
         return [$upload, $url];
     }
 
     protected function checkHashConflicts(Sha1Hash $hash, int $length): void
     {
-        $file = File::where('hash', $hash->raw)->first();
+        $file = File::where('hash', $hash->toString())->first();
         if ($file !== null) {
             // TODO[pn]: hash collision
             //if ($file->length !== $length) {
             throw new AlreadyUploadedException($file);
         }
 
-        $upload = Upload::where('hash', $hash->raw)->first();
+        $upload = Upload::where('hash', $hash->toString())->first();
         if ($upload !== null) {
             // TODO[pn]: hash collision
             //if ($upload->length !== $length) {

app/Support/Id.php
@@ -3,7 +3,7 @@ namespace App\Support;
 
 class Id
 {
-    const MIN = -9223372036854775808;
+    const MIN = -9223372036854775807;
     const MAX = 9223372036854775807;
     public static function generate(): int
     {

app/Support/Mixin/OptionallyBeginsTransaction.php
@@ -9,7 +9,7 @@ trait OptionallyBeginsTransaction
     protected function inTransaction(\Closure $callback)
     {
         if ($this->databaseTransactionsManager->getTransactions()->isEmpty()) {
-            return DB::transaction($callback);
+            return app('db')->transaction($callback);
         } else {
             return $callback();
         }

app/Support/RpcConstants.php
@@ -11,4 +11,7 @@ class RpcConstants
         ERROR_UNAUTHORIZED = self::H + Response::HTTP_UNAUTHORIZED,
         ERROR_NOT_FOUND = self::H + Response::HTTP_NOT_FOUND,
         ERROR_CONFLICT = self::H + Response::HTTP_CONFLICT;
+
+    public const
+        ERROR_INVALID_PARAMS = -32602;
 }

app/Support/Sha1Hash.php
@@ -31,13 +31,14 @@ class Sha1Hash
     // MARK: StringableCast conformance
     public static function fromString(string $raw): self
     {
-        if (strlen($raw) !== 20) {
+        if (substr($raw, 0, 2) !== '\x' || strlen($raw) !== 42) {
             throw new \InvalidArgumentException();
         }
-        return new self($raw);
+        $hex = substr($raw, 2);
+        return new self(hex2bin($hex));
     }
     public function toString(): string
     {
-        return $this->raw;
+        return '\x' . bin2hex($this->raw);
     }
 }